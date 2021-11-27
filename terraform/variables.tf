941c95cf71456b57b58e07433d44114db80877b3
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2021-11-27 11:25:58 +0000

    feat(schedule): add multi-schedule support (#2)
    * feat(schedules): add multi schedules to execute orders
    
    * docs(schedule): added schedule section to readme
    
    * docs(readme): typo
    
    * chore(schedule): adjust

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+// SCHEDULES
+
+variable "execute_orders_schedules" {
+  type = list(object({
+    description         = string
+    schedule_expression = string
+    # https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
+    # https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents-expressions.html
+  }))
+
+  default = [
+    {
+      description         = "At 6:00 UTC on every Friday"
+      schedule_expression = "cron(0 6 ? * FRI *)"
+    }
+  ]
+}
