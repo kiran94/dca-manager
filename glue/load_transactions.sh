fc0954af72354b62706965fc8b02c81d7fcb6da0
kiran94 <kiran_patel94@hotmail.com>
2021-12-20 23:50:46 +0000

    refactor(glue): update test script

--- glue/load_transactions.sh
+++ glue/load_transactions.sh
@@ -1,10 +1,8 @@
+#!/bin/bash
+# NOTE: this is just a quick testing script
+# https://hudi.apache.org/docs/quick-start-guide/
 
 spark-submit \
-  --packages org.apache.hudi:hudi-spark3-bundle_2.12:0.8.0,org.apache.spark:spark-avro_2.12:3.0.1 \
-  --conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer' ./scripts/load_transactions.py
-
-
-
-  # --py-files ../../aws-glue-libs/
-# hudi-spark-bundle_2.11-0.8.0.jar
-# spark-avro_2.11-2.4.7.jar
+    --packages org.apache.hudi:hudi-spark3-bundle_2.12:0.10.0,org.apache.spark:spark-avro_2.12:3.1.2 \
+    --conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer' \
+    ./scripts/load_transactions.py
